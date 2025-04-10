7c39dad0a20efeac0bb7976dc93f949987d7a4d1
TamarHayat <tahayat@g.jct.ac.il>
2025-04-10 16:18:03 +0300

    add language switchers

--- full_stack_4/src/components/EditorArea.jsx
+++ full_stack_4/src/components/EditorArea.jsx
@@ -1,12 +1,30 @@
 import VirtualKeyboard from "./VirtualKeyboard";
 import DeleteKeys from "./DeleteKeys";
+import KeyboardSwitcher from "./KeyboardSwitcher";
+import { useState } from "react";
+
 
 function EditorArea({ onKeyPress, onDeleteAll, onDeleteChar }) {
+  const [currentLayout, setCurrentLayout] = useState("english");
+
+  const handleSwitchLayout = (layout) => {
+    setCurrentLayout(layout);
+  };
   return (
     <div className="editor-area">
       {/* add here more edit components */}
-      <VirtualKeyboard onKeyPress={onKeyPress} />
-      <DeleteKeys onDeleteAll={onDeleteAll} onDeleteChar={onDeleteChar} />
+      <KeyboardSwitcher
+        currentLayout={currentLayout}
+        onSwitchLayout={handleSwitchLayout}
+      />
+      <VirtualKeyboard 
+        onKeyPress={onKeyPress} 
+        layout={currentLayout}
+      />
+      <DeleteKeys 
+        onDeleteAll={onDeleteAll} 
+        onDeleteChar={onDeleteChar} 
+      />
     </div>
   );
 }
